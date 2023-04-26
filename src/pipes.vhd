fad7877e52620be54bb614e9344d418ec8b55721
fosu562 <100399572+fosu562@users.noreply.github.com>
2023-04-27 10:32:51 +1200

    feat :sparkles: Successfully generating two seperate pipes, both of which are able to move and reset when they reach the end of the page

--- src/pipes.vhd
+++ src/pipes.vhd
@@ -13,31 +13,40 @@ entity pipes is
 end pipes;
 
 architecture behavior of pipes is
-    constant PIPE_GAP : std_logic_vector(9 downto 0) := conv_std_logic_vector(100, 10);
-    constant PIPE_GAP_POSITION : std_logic_vector(9 downto 0) := conv_std_logic_vector(220, 10);
+    constant PIPE_GAP : std_logic_vector(9 downto 0) := conv_std_logic_vector(100, 10); -- Define the size of the gap between the pipes
+    constant PIPE_GAP_POSITION_A : std_logic_vector(9 downto 0) := conv_std_logic_vector(220, 10); -- Position of the gap between the pipes
+    constant PIPE_GAP_POSITION_B : std_logic_vector(9 downto 0) := conv_std_logic_vector(220, 10); -- Position of the gap between the pipes
     constant PIPE_WIDTH : std_logic_vector(9 downto 0) := conv_std_logic_vector(20, 10);
     -- constant ACCELLERATION : std_logic_vector(9 downto 0) := conv_std_logic_vector(0, 0);
 
-	signal L_X_POS : std_logic_vector(10 downto 0) := CONV_STD_LOGIC_VECTOR(639, 11);
-	signal L_Y_POS : std_logic_vector(9 downto 0) := CONV_STD_LOGIC_VECTOR(479, 10);
+	signal L_X_POS_A : std_logic_vector(10 downto 0) := CONV_STD_LOGIC_VECTOR(640, 11);
+	signal L_X_POS_B : std_logic_vector(10 downto 0) := CONV_STD_LOGIC_VECTOR(960, 11);
 
 begin
 	Move_pipes : process (I_V_SYNC)
-		variable X_POS : std_logic_vector(10 downto 0) := CONV_STD_LOGIC_VECTOR(639, 11);
-		variable X_VEL : std_logic_vector(9 downto 0) := CONV_STD_LOGIC_VECTOR(1, 10);
+		variable X_POS_A : std_logic_vector(10 downto 0) := CONV_STD_LOGIC_VECTOR(640, 11);
+		variable X_POS_B : std_logic_vector(10 downto 0) := CONV_STD_LOGIC_VECTOR(960, 11);
+		variable X_VEL : std_logic_vector(9 downto 0) := CONV_STD_LOGIC_VECTOR(2, 10);
 	begin
-		-- Move pipe once every vertical sync
+		-- Move pipes once every vertical sync
 		if (rising_edge(I_V_SYNC)) then
-			X_POS := L_X_POS - X_VEL;
-            if (X_POS <= 0) then
-                X_POS := CONV_STD_LOGIC_VECTOR(639, 11);
+			X_POS_A := L_X_POS_A - X_VEL;
+			X_POS_B := L_X_POS_B - X_VEL;
+			-- If the pipes overflow, place them back at the start
+            if (X_POS_A <= 0) then
+                X_POS_A := CONV_STD_LOGIC_VECTOR(640, 11);
+            elsif (X_POS_B <= 0) then
+                X_POS_B := CONV_STD_LOGIC_VECTOR(640, 11);
             end if;
-			L_X_POS <= X_POS;
+			L_X_POS_A <= X_POS_A;
+			L_X_POS_B <= X_POS_B;
 		end if;
 	end process Move_pipes;
 
-	O_ON <= '1' when (('0' & L_X_POS <= '0' & I_PIXEL_COL + PIPE_WIDTH) and ('0' & I_PIXEL_COL <= '0' & L_X_POS + PIPE_WIDTH) -- x_pos - size <= pixel_column <= x_pos + size
-		and (('0' & I_PIXEL_ROW <= PIPE_GAP_POSITION - PIPE_GAP) or  ('0' & I_PIXEL_ROW >= PIPE_GAP_POSITION + PIPE_GAP))) else -- y_pos - size <= pixel_row <= y_pos + size
+	O_ON <= '1' when (((('0' & I_PIXEL_COL >= '0' & L_X_POS_A - PIPE_WIDTH) and ('0' & I_PIXEL_COL <= '0' & L_X_POS_A + PIPE_WIDTH))-- L_X_POS_A - PIPE_WIDTH <= I_PIXEL_COL <= L_X_POS_A + PIPE_WIDTH
+			and (('0' & I_PIXEL_ROW <= PIPE_GAP_POSITION_A - PIPE_GAP) or ('0' & I_PIXEL_ROW >= PIPE_GAP_POSITION_A + PIPE_GAP)))  -- PIPE_GAP_POSITION_A - PIPE_GAP >= I_PIXEL_ROW >= PIPE_GAP_POSITION_A + PIPE_GAP
+		or ((('0' & I_PIXEL_COL >= '0' & L_X_POS_B - PIPE_WIDTH) and ('0' & I_PIXEL_COL <= '0' & L_X_POS_B + PIPE_WIDTH)) -- L_X_POS_B - PIPE_WIDTH <= I_PIXEL_COL <= L_X_POS_B + PIPE_WIDTH)			
+			and (('0' & I_PIXEL_ROW <= PIPE_GAP_POSITION_B - PIPE_GAP) or ('0' & I_PIXEL_ROW >= PIPE_GAP_POSITION_B + PIPE_GAP)))) else  -- PIPE_GAP_POSITION_B - PIPE_GAP >= I_PIXEL_ROW >= PIPE_GAP_POSITION_B + PIPE_GAP 
 		'0';
 
 	O_RGB <= x"5E2";
