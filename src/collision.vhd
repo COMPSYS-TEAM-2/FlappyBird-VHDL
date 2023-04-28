2037ff651b9faa38a2f4a2228a6ab214f4338b1a
fosu562 <Franklino0808@gmail.com>
2023-04-28 21:21:56 +1200

    perf: :zap: Prevented pipes from giving multiple collision instances

--- src/collision.vhd
+++ src/collision.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity collision is
     port (
-        I_CLK, I_S_ON, I_P_ON : in std_logic;
+        I_HSYNC, I_S_ON, I_P_ON : in std_logic;
         O_COL : out std_logic;
     );
 end collision;
@@ -12,11 +12,15 @@ end collision;
 architecture behaviour of collision is
 
     signal COL : std_logic;
+    variable counter : std_logic(4 downto 0);
 begin
-    check - collision : process (I_CLK) is
-    if ((I_S_ON = '1') and (I_P_ON = '1')) then
-        COL <= '1';
-    end if;
-end process
-O_COL <= COL;
+    checkCollision : process (I_HSYNC) is
+        if ((I_S_ON = '1') and (I_P_ON = '1') and (counter = (conv_std_logic_vector(0, 5)))) then
+            COL <= '1';
+            counter := conv_std_logic_vector(20, 5);
+        elsif (counter > conv_std_logic_vector(0, 5)) then
+            counter := counter - conv_std_logic_vector(1, 5);
+        end if;
+    end process
+    O_COL <= COL;
 end architecture;
